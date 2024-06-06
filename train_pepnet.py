ee9b9322731b920df61a13cf55c561116ba2ec88
chenjiawei <chenjiawei@chenjiaweis-MacBook-Pro.local>
2024-06-06 16:35:47 +0800

    u

--- train_pepnet.py
+++ train_pepnet.py
@@ -1,11 +1,22 @@
 import tensorflow as tf
 import joblib
 from sklearn.metrics import roc_auc_score
+import yaml
 
-write_features_map_path = "../mtl/" + 'features_map.pkl'
+
+"""
+alicpp数据格式
+    sample_skeleton: sampleID (样本编码),click,conversion,common_feature_index,特征域数量, 特征列表
+        特征列表：field_id, feature_id, feature_value
+    common_feature_index: 主要是用户侧特征
+        index,特征数,特征列表：field_id, feature_id, feature_value
+
+"""
 
 
-vocabulary = joblib.load(write_features_map_path)
 
-print(vocabulary.keys())
+if __name__ == "__main__":
+    with open("../config/pepnet.yaml", "r") as fd:
+        model_config = yaml.safe_load(fd)
 
+    print(model_config)
