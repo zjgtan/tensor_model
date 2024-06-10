37227934cd634319feb47d34832b3e63c585300d
chenjiawei <chenjiawei@chenjiaweis-MacBook-Pro.local>
2024-06-10 09:45:13 +0800

    u

--- train_pepnet.py
+++ train_pepnet.py
@@ -42,13 +42,10 @@ def train_epoch(model, dataset, optimizer):
     idx = 0
     for batch in tqdm(dataset):
         with tf.GradientTape() as tape:
-            click_logits, conversion_logits = model(batch)
-            click_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch["click"], logits=click_logits))
-            conversion_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch["conversion"], logits=conversion_logits))
-
-            click_auc = roc_auc_score(batch["click"], tf.nn.sigmoid(click_logits))
-
-            loss = click_loss + conversion_loss
+            output_logits = model(batch)
+            click_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch["click"], logits=output_logits[0]))
+            click_auc = roc_auc_score(batch["click"], tf.nn.sigmoid(output_logits[0]))
+            loss = click_loss
 
         gradients = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -58,6 +55,27 @@ def train_epoch(model, dataset, optimizer):
 
         idx += 1
 
+def eval(model, dataset):
+    click_logits_list, conversion_logits_list, click_list, conversion_list = [], [], [], []
+
+    for batch in tqdm(dataset):
+        #click_logits, conversion_logits = model(batch)
+        output_logits = model(batch)
+        click_logits_list.append(output_logits[0])
+        #conversion_logits_list.append(conversion_logits)
+        click_list.append(batch["click"])
+        #conversion_list.append(batch["conversion"])
+
+
+    all_click_logits = tf.concat(click_logits_list, axis=0)
+    #all_conversion_logits = tf.concat(conversion_logits_list, axis=0)
+    all_click = tf.concat(click_list, axis=0)
+    #all_conversion = tf.concat(conversion_list, axis=0)
+
+    click_auc = roc_auc_score(all_click, tf.nn.sigmoid(all_click_logits))
+    #conversion_auc = roc_auc_score(all_conversion, tf.nn.sigmoid(all_conversion_logits))
+
+    print("test: ", click_auc)
 
 if __name__ == "__main__":
     with open("./config/pepnet.yaml", "r") as fd:
@@ -71,13 +89,13 @@ if __name__ == "__main__":
     optimizer = tf.keras.optimizers.Adam()
 
     # 定义输入输出数据流
-    #alicpp_train_set = tf.data.TFRecordDataset(["../mtl/train.tfrecord"]).map(lambda record: parse_example(record, feature_map)).batch(yaml_config["batch_size"])
     alicpp_train_set = tf.data.TFRecordDataset(["../mtl/train.tfrecord"]).map(lambda record: parse_example(record, feature_map), num_parallel_calls=10).apply(tf.data.experimental.dense_to_ragged_batch(batch_size=yaml_config["batch_size"])).prefetch(10)
-
+    alicpp_test_set = tf.data.TFRecordDataset(["../mtl/test.tfrecord"]).map(lambda record: parse_example(record, feature_map)).apply(tf.data.experimental.dense_to_ragged_batch(batch_size=yaml_config["batch_size"]))
 
     for epoch in range(yaml_config["epoch"]):
         train_epoch(net, alicpp_train_set, optimizer)
 
+    eval(net, alicpp_test_set)
 
 
 
